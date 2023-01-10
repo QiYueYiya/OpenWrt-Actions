aef84a081738a18179add65bfcd4c79439c6c5af
QiYueYiya <qiyueyi@pm.me>
2023-01-10 08:43:57 +0800

    update

--- X86_64-School/diy2.sh
+++ X86_64-School/diy2.sh
@@ -9,7 +9,7 @@ echo "echo 'iptables -t nat -A ntp_force_local -d 192.168.0.0/16 -j RETURN' >> /
 echo "echo 'iptables -t nat -A ntp_force_local -s 192.168.0.0/16 -j DNAT --to-destination 192.168.3.1' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
 echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='OpenWrt'/hostname='DESKTOP-RFK5J88'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='DESKTOP'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 # 调整 V2ray服务器 到 VPN 菜单
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
