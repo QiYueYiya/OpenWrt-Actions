a7a64cb2d15be09a2eedc25fd39083e8994c09c8
QiYueYiya <qiyueyi@pm.me>
2023-01-13 20:15:09 +0800

    update

--- X86_64-Sch/diy2.sh
+++ X86_64-Sch/diy2.sh
@@ -1,13 +1,4 @@
-sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t mangle -A POSTROUTING -j TTL --ttl-set 128' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -N ntp_force_local' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -I PREROUTING -p udp --dport 123 -j ntp_force_local' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -A ntp_force_local -d 0.0.0.0/8 -j RETURN' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -A ntp_force_local -d 127.0.0.0/8 -j RETURN' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -A ntp_force_local -d 192.168.0.0/16 -j RETURN' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'iptables -t nat -A ntp_force_local -s 192.168.0.0/16 -j DNAT --to-destination 192.168.3.1' >> /etc/firewall.user" >> package/lean/default-settings/files/zzz-default-settings
-echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='DESKTOP'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
