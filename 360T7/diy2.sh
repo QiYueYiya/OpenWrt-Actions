232963c65583a517094972c8a3a6091d44afe755
QiYueYiya <qiyueyi@pm.me>
2023-01-13 20:29:30 +0800

    update

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='Pardofelis and QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
--- X86_64/diy2.sh
+++ X86_64/diy2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='Pardofelis and QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
