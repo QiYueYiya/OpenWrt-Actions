137cf3334291875bc2ce869c1dd0d6e9452d8be8
QiYueYiya <qiyueyi@pm.me>
2023-01-27 15:36:19 +0800

    update

--- 360T7-Sch/diy1.sh
+++ 360T7-Sch/diy1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-# sed -i '$a src-git-full qiyueyi https://github.com/QiYueYiya/openwrt-package.git' feeds.conf.default
+sed -i '$a src-git-full qiyueyi https://github.com/QiYueYiya/openwrt-package.git' feeds.conf.default
 # theme
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
