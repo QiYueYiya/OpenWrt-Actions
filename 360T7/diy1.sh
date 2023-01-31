cb573709c7eba8c9d41771935e3674cb41a2b5ad
QiYueYiya <qiyueyi@pm.me>
2023-01-31 20:53:53 +0800

    update

--- 360T7/diy1.sh
+++ 360T7/diy1.sh
@@ -14,7 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a src-git-full qiyueyi https://github.com/QiYueYiya/openwrt-package.git' feeds.conf.default
 # theme
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
+svn co https://github.com/shidahuilang/openwrt-package/branches/openwrt-21.02/adguardhome package/adguardhome
+svn co https://github.com/shidahuilang/openwrt-package/branches/openwrt-21.02/aliyundrive-webdav package/aliyundrive-webdav
+svn co https://github.com/shidahuilang/openwrt-package/branches/openwrt-21.02/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/shidahuilang/openwrt-package/branches/openwrt-21.02/luci-app-aliyundrive-webdav package/luci-app-aliyundrive-webdav
+svn co https://github.com/shidahuilang/openwrt-package/branches/openwrt-21.02/luci-app-wolplus package/luci-app-wolplus
