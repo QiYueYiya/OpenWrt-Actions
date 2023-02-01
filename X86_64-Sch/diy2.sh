65ef4a35527409569a26b117b0881833a3116ad7
QiYueYiya <qiyueyi@pm.me>
2023-02-01 19:13:39 +0800

    update

--- X86_64-Sch/diy2.sh
+++ X86_64-Sch/diy2.sh
@@ -20,7 +20,7 @@ echo "DISTRIB_MODEL='X86_64-Sch'" >> package/base-files/files/etc/openwrt_releas
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
-target=$(grep -m 1 "CONFIG_TARGET_.*_.*" .config | sed "s/CONFIG_TARGET_\(.*\)_\(.*\)=y/\1\/\2/g")
+target=$(grep -m 1 "CONFIG_TARGET_.*_.*=y" .config | sed "s/CONFIG_TARGET_\(.*\)_\(.*\)=y/\1\/\2/g")
 for configFile in $(ls target/linux/$target/config*)
 do
     echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
