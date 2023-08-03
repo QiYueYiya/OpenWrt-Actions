f423e4310c405ad7f29cda906de21a56a0e24288
QiYueYiya <pardofeliss@outlook.com>
2023-08-03 11:06:24 +0800

    更新软件源

--- X86_64/diy1.sh
+++ X86_64/diy1.sh
@@ -12,15 +12,15 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 软件包
-git clone --depth 1 https://github.com/Zxilly/UA2F package/UA2F
-git clone --depth 1 https://github.com/pymumu/openwrt-smartdns.git package/smartdns
-git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth 1 https://github.com/Zxilly/UA2F package/UA2F
+# git clone --depth 1 https://github.com/pymumu/openwrt-smartdns.git package/smartdns
+# git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+# svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 
 # 主题
 git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
