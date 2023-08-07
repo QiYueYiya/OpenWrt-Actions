1c9a9acdb787b5a09f190c761dae2d817d9ae24f
QiYueYiya <pardofeliss@outlook.com>
2023-08-07 16:32:35 +0800

    添加Lienol编译

--- Lienol/diy1.sh
+++ Lienol/diy1.sh
@@ -1,1 +1,4 @@
-sed -i '$a src-git smallpackage https://github.com/kenzok8/small-package' feeds.conf.default
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall-packages
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
