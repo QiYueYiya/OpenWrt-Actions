6d467427431a28ba1377094698a3e60640ba08e4
QiYueYiya <pardofeliss@outlook.com>
2023-08-07 22:30:31 +0800

    删除Lienol

--- Lienol/diy1.sh
+++
@@ -1,4 +0,0 @@
-git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall-packages
-git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
--- Lienol/diy2.sh
+++
@@ -1,8 +0,0 @@
-# 修改默认IP地址
-sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
-# 修改主机名
-sed -i "s/hostname='OpenWrt'/hostname='Pardofelis'/g" package/base-files/files/bin/config_generate
-# 修改设备说明
-sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
