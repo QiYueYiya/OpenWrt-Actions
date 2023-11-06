aca58de88cb0a00b6b62288ba0c1aced970e1131
QiYueYiya <pardofeliss@outlook.com>
2023-11-06 18:19:43 +0800

    修改

--- immortalwrt/diy1.sh
+++ immortalwrt/diy1.sh
@@ -15,6 +15,3 @@
 # sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 软件包
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
--- immortalwrt/diy2.sh
+++ immortalwrt/diy2.sh
@@ -13,7 +13,5 @@
 # 修改默认IP地址
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.252.0/g' package/base-files/files/bin/config_generate
-# 修改主机名
-sed -i "s/hostname='OpenWrt'/hostname='Pardofelis'/g" package/base-files/files/bin/config_generate
 # 修改设备说明
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
