6bbdc015c190e26618d35051814f3a74865c03ed
QiYueYiya <pardofeliss@outlook.com>
2025-02-04 21:44:15 +0800

    修改设备名称

--- 360T7/diy2.sh
+++ 360T7/diy2.sh
@@ -15,5 +15,5 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='Pardofelis'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
