88d0e04679b0a4317d870c352dc95aff7b789e48
QiYueYiya <pardofeliss@outlook.com>
2025-02-05 15:44:52 +0800

    添加IP变量删除无用命令

--- immortalwrt-23/diy2.sh
+++ immortalwrt-23/diy2.sh
@@ -11,8 +11,8 @@
 #
 
 # 修改默认IP地址
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i 's/255.255.255.0/255.255.255.0/g' package/base-files/files/bin/config_generate
+host_ip="192.168.6.1"
+sed -i 's/192.168.1.1/${host_ip}/g' package/base-files/files/bin/config_generate
 # 修改设备说明
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 # OpenClash
