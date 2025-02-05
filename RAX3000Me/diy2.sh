4cc032916ca06897a3455f52e6d9f1534b40ca3d
QiYueYiya <pardofeliss@outlook.com>
2025-02-05 15:32:18 +0800

    添加主机名和IP变量

--- RAX3000Me/diy2.sh
+++ RAX3000Me/diy2.sh
@@ -10,6 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000Me'/g" package/base-files/files/bin/config_generate
+host_name="RAX3000Me"
+host_ip="192.168.5.1"
+sed -i "s/192.168.1.1/${host_ip}/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='ImmortalWrt'/hostname='${host_name}'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
