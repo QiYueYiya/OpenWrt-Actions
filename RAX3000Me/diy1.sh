4fa265ff31ab6b1fa75281104bda40c6fd6de904
QiYueYiya <pardofeliss@outlook.com>
2025-02-06 22:16:56 +0800

    再次尝试编译Mosdns插件

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -17,7 +17,8 @@
 # AdGuardHome
 git clone --depth 1 -b luci-app-adguardhome https://github.com/QiYueYiya/OpenWrt-Actions package/luci-app-adguardhome
 # Mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
