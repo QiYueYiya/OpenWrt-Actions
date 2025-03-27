01cf20c526eb25013cdbd01b90be8f2ee90b8fce
QiYueYiya <pardofeliss@outlook.com>
2025-03-27 19:30:41 +0800

    添加toolchain工具链

--- RAX3000Me/diy2.sh
+++ RAX3000Me/diy2.sh
@@ -16,3 +16,7 @@ sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'
 
 wget -O llvm-bpf.tar.zst https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/targets/mediatek/filogic/llvm-bpf-18.1.7.Linux-x86_64.tar.zst
 tar -xvaf llvm-bpf.tar.zst
+wget -O toolchain-mediatek-filogic.tar.zst https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/targets/mediatek/filogic/immortalwrt-toolchain-24.10-SNAPSHOT-mediatek-filogic_gcc-13.3.0_musl.Linux-x86_64.tar.zst
+mkdir -p toolchain-mediatek-filogic
+tar -xvf toolchain-mediatek-filogic.tar.zst -C toolchain-mediatek-filogic --strip-components=1
+./scripts/ext-toolchain.sh --toolchain toolchain-mediatek-filogic/toolchain* --overwrite-config --config mediatek/filogic
