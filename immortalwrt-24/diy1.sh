b5598c43b995c17dc6c062df5f8e610a2f76ca67
QiYueYiya <pardofeliss@outlook.com>
2025-04-12 11:40:30 +0800

    编译24-master分支

--- immortalwrt-24/diy1.sh
+++ immortalwrt-24/diy1.sh
@@ -17,7 +17,4 @@
 # 自定义软件源
 git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages.git package/openwrt-packages
 # Easytier
-git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier # 临时存放easytier
-mv package/luci-app-easytier/easytier package/openwrt-packages/easytier # 移动easytier到openwrt-packages
-mv package/luci-app-easytier/luci-app-easytier package/openwrt-packages/luci-app-easytier # 移动luci-app-easytier到openwrt-packages
-rm -rf package/luci-app-easytier # 删除临时存放easytier
+git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
