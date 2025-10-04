ccd93e56b2f766ee52d02a063ffe9b45e974eb9c
QiYueYiya <pardofeliss@outlook.com>
2025-10-04 10:33:48 +0800

    修改主IP地址

--- build/mediatek/filogic/360T7/diy_device.sh
+++ build/mediatek/filogic/360T7/diy_device.sh
@@ -4,6 +4,6 @@
 #####################################################
 #
 #
-sed -i "s/192.168.1.1/"192.168.4.1"/g" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.5.1/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
