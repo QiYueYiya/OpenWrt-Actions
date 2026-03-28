b3e93f1490f2266b045d434e2207e819f6812bf7
QiYueYiya <pardofeliss@outlook.com>
2026-03-28 10:49:56 +0800

    golang使用默认分支

--- build/mediatek/filogic/diy_package.sh
+++ build/mediatek/filogic/diy_package.sh
@@ -13,6 +13,6 @@ git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git pa
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
