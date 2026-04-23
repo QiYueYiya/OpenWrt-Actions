0f05cee712dba505d4aa929979b210eaf4142be6
QiYueYiya <pardofeliss@outlook.com>
2026-04-23 21:29:15 +0800

    添加X86设备

---
+++ build/generic/config.sh
@@ -0,0 +1,12 @@
+# 设备名称，必须与官方设备名称一致
+export profile="generic"
+# 设备架构，必须与官方设备架构一致
+export targets="x86/64"
+# 设备CPU架构（用于下载预编译包）
+export arch="x86_64"
+# OpenWrt版本，必须与官方版本一致
+export version="24.10.5"
+# 后台管理IP地址
+export ip_address="172.16.0.2"
+# 网络掩码
+export netmask="255.255.255.0"
